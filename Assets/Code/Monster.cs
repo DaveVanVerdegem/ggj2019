0278b669ed26be4fde0453e7619c99d196d40bd6
Dave Van Verdegem <dave@pajamallama.be>
2019-01-26 00:21:14 +0100

    * Updated test scene. * Added action and manager objects.

---
+++ Assets/Code/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+	#region Life Cycle
+	// Start is called before the first frame update
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Registers an action taken on the monster.
+	/// </summary>
+	/// <param name="inputType">Type of input used.</param>
+	/// <param name="hotSpot">Hot spot where the input was used on.</param>
+	public void RegisterAction(ActionType inputType, HotSpot hotSpot)
+	{
+		Debug.Log(string.Format("Registering input of {0} at {1}.", inputType, hotSpot), this);
+	}
+	#endregion
+}
