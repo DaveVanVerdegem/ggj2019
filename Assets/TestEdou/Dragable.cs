d84ec0115e806e892f921e11ecd7fa79ef939cfe
edoubits <edouardvb@gmail.com>
2019-01-26 00:22:32 +0100

    commit

--- Assets/TestEdou/Dragable.cs
+++ Assets/TestEdou/Dragable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(SpriteRenderer))]
-public class HighlightOnCollide : MonoBehaviour
+public class Dragable : MonoBehaviour
 {
     public Color HighlightColor;
     Color originalColor;
@@ -27,10 +27,18 @@ public class HighlightOnCollide : MonoBehaviour
     void OnMouseEnter()
     {
         originalColor = spr.color;
-        spr.color = HighlightColor;
+        if (HighlightColor.a>0) spr.color = HighlightColor;
     }
     void OnMouseExit()
     {
         spr.color = originalColor;
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        objPosition.z = transform.position.z;
+
+        transform.position = objPosition;
     }
 }
