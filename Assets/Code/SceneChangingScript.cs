df5b291cc65db7c4444e2d696c9f6355fe120c34
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2019-01-25 23:14:49 +0100

    Change to bedroom scene on startup
    Not sure what the best practices are so I left some code in comment.

---
+++ Assets/Code/SceneChangingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangingScript : MonoBehaviour
+{
+
+    //public Scene startScene;
+    public string startSceneName;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //SceneManager.LoadScene(startScene.name, LoadSceneMode.Single);
+        SceneManager.LoadScene(startSceneName, LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
