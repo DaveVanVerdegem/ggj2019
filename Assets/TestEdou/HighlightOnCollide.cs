3dba742096034a5dc75fa407ab65527b59d2120b
edoubits <edouardvb@gmail.com>
2019-01-25 23:18:22 +0100

    Test code

---
+++ Assets/TestEdou/HighlightOnCollide.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class HighlightOnCollide : MonoBehaviour
+{
+    public Color HighlightColor;
+    Color originalColor;
+    Collider col;
+    SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider>();
+        spr= GetComponent<SpriteRenderer>();
+        originalColor = spr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        originalColor = spr.color;
+        spr.color = HighlightColor;
+    }
+    void OnMouseExit()
+    {
+        spr.color = originalColor;
+    }
+}
