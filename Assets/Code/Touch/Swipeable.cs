43e967d06eae792c2f2d94923a96797a1b709e96
Dave Van Verdegem <dave@pajamallama.be>
2019-01-26 16:25:18 +0100

    * Starting to bring components together.

--- Assets/Code/Touch/Swipeable.cs
+++ Assets/Code/Touch/Swipeable.cs
@@ -3,84 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-    [RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class Swipeable : MonoBehaviour
 {
-    [Tooltip("Optional log output")]
-    public Text log=null;
-    [Tooltip("Minimum seconds delay between 2 swipes that swipes are counted")]
-    public float minDelayBetweenSwipes = 0.05f;
-    [Tooltip("Maximum seconds delay between 2 swipes that swipes are counted before resetting the swipe count")]
-    public float maxDelayBetweenSwipes = 0.5f;
-    [Tooltip("Minimum squared distance between touch end and start to be counted as a swipe")]
-    public int minDistanceForSwipe = 450;
-    [Tooltip("Swipe when dragging finger (instead of lifting finger)")]
-    public bool swipeDrag=false;
+	[Tooltip("Optional log output")]
+	public Text log = null;
+	[Tooltip("Minimum seconds delay between 2 swipes that swipes are counted")]
+	public float minDelayBetweenSwipes = 0.05f;
+	[Tooltip("Maximum seconds delay between 2 swipes that swipes are counted before resetting the swipe count")]
+	public float maxDelayBetweenSwipes = 0.5f;
+	[Tooltip("Minimum squared distance between touch end and start to be counted as a swipe")]
+	public int minDistanceForSwipe = 450;
+	[Tooltip("Swipe when dragging finger (instead of lifting finger)")]
+	public bool swipeDrag = false;
 
-    CircleCollider2D col;
-    private float distanceSwiped;
-    private int swipeCount;
-    private float sinceLastSwipe;
-    private Touch touch;
+	private Collider2D col;
+	private float distanceSwiped;
+	private int swipeCount;
+	private float sinceLastSwipe;
+	private Touch touch;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        col = GetComponent<CircleCollider2D>();
-        distanceSwiped = 0;
-        swipeCount = 0;
-        sinceLastSwipe = Time.time;
-    }
+	// Start is called before the first frame update
+	private void Start()
+	{
+		col = GetComponent<Collider2D>();
+		distanceSwiped = 0;
+		swipeCount = 0;
+		sinceLastSwipe = Time.time;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Touch[] touches = Input.touches;
-        for (int touchIndex = 0; touchIndex < touches.Length; touchIndex++)
-        {
-            Vector3 touchWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(touches[touchIndex].position.x, touches[touchIndex].position.y, 0));
-            //if (log) log.text = "\n col.bounds " + col.bounds+ "\n touches[touchIndex].position " + touches[touchIndex].position + "\n touchWorldPoint " + touchWorldPoint;
-            if (col.OverlapPoint(touchWorldPoint))
-            {
-                if ((swipeDrag && touches[touchIndex].phase == TouchPhase.Moved)
-               || (!swipeDrag && touches[touchIndex].phase == TouchPhase.Ended))
-                {
-                    distanceSwiped += touches[touchIndex].deltaPosition.sqrMagnitude;
-                    if (distanceSwiped > minDistanceForSwipe)
-                    {
-                        if ((sinceLastSwipe < Time.time + maxDelayBetweenSwipes)
-                            && (sinceLastSwipe + minDelayBetweenSwipes < Time.time))
-                        {
-                            distanceSwiped %= minDistanceForSwipe;
-                            touch = touches[touchIndex];
-                            if (log) log.text = "\n Swiped! \n" + GetDebugInfo(touch);
-                            swipeCount++;
-                        }
-                        sinceLastSwipe = Time.time;
-                    }
-                }
-            }
-        }
-        if (sinceLastSwipe + maxDelayBetweenSwipes < Time.time )
-        {
-            distanceSwiped = 0;
-               swipeCount = 0;
-            if (log) log.text = "\n Reset! \n" + GetDebugInfo(touch);
-        }
-    }
+	// Update is called once per frame
+	private void Update()
+	{
+		Touch[] touches = Input.touches;
+		for (int touchIndex = 0; touchIndex < touches.Length; touchIndex++)
+		{
+			Vector3 touchWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(touches[touchIndex].position.x, touches[touchIndex].position.y, 0));
+			//if (log) log.text = "\n col.bounds " + col.bounds+ "\n touches[touchIndex].position " + touches[touchIndex].position + "\n touchWorldPoint " + touchWorldPoint;
+			if (col.OverlapPoint(touchWorldPoint))
+			{
+				if ((swipeDrag && touches[touchIndex].phase == TouchPhase.Moved)
+			   || (!swipeDrag && touches[touchIndex].phase == TouchPhase.Ended))
+				{
+					distanceSwiped += touches[touchIndex].deltaPosition.sqrMagnitude;
+					if (distanceSwiped > minDistanceForSwipe)
+					{
+						if ((sinceLastSwipe < Time.time + maxDelayBetweenSwipes)
+							&& (sinceLastSwipe + minDelayBetweenSwipes < Time.time))
+						{
+							distanceSwiped %= minDistanceForSwipe;
+							touch = touches[touchIndex];
+							if (log) log.text = "\n Swiped! \n" + GetDebugInfo(touch);
+							swipeCount++;
+						}
+						sinceLastSwipe = Time.time;
+					}
+				}
+			}
+		}
+		if (sinceLastSwipe + maxDelayBetweenSwipes < Time.time)
+		{
+			distanceSwiped = 0;
+			swipeCount = 0;
+			if (log) log.text = "\n Reset! \n" + GetDebugInfo(touch);
+		}
+	}
 
-    public double NumberOfSwipes()
-    {
-        return swipeCount;
-    }
+	public double NumberOfSwipes()
+	{
+		return swipeCount;
+	}
 
-    public Touch LastTouch()
-    {
-        return touch;
-    }
+	public Touch LastTouch()
+	{
+		return touch;
+	}
 
-    public string GetDebugInfo(Touch t)
-    {
-        return "SwipeCount " + swipeCount.ToString() + "\nPosition" + t.position + "\nAngle" + (t.deltaPosition.x/ t.deltaPosition.y) + "\nDeltaPosition" + t.deltaPosition;
-    }
+	public string GetDebugInfo(Touch t)
+	{
+		return "SwipeCount " + swipeCount.ToString() + "\nPosition" + t.position + "\nAngle" + (t.deltaPosition.x / t.deltaPosition.y) + "\nDeltaPosition" + t.deltaPosition;
+	}
 }
