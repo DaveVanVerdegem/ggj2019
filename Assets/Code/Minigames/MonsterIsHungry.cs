eeb71b769502c00e077dfd01cd6c1a42462bf92d
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2019-01-26 16:36:29 +0100

    Monster is hungry minigame. monster eats food
    Still needs to properly propagate win state

--- Assets/Code/Minigames/MonsterIsHungry.cs
+++ Assets/Code/Minigames/MonsterIsHungry.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The monster is hungry. Feed him food!
+/// </summary>
 public class MonsterIsHungry : MonoBehaviour
 {
+
+    public GameObject hotspot;
+
+    public ActionQueueProperties actionQueueProperties;
+
+    private Collider2D mouthCollider;
+    private InputTrigger inputTrigger;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mouthCollider = GetComponent<Collider2D>();
+        inputTrigger = hotspot.GetComponent<InputTrigger>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //drag items tagged as food on the monsters mouth
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("in collider", this);
+
+        if (other.gameObject.CompareTag(Tags.FOOD))
+        {
+            Debug.Log("Food in mouth!", this);
+            Destroy(other.gameObject);
+        }
+    }
+
+    void CompleteMinigame()
+    {
+        Debug.Log("The monster is fed and happy!", this);
     }
 }
