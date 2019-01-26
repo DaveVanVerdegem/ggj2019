7d031dd932b6bc21e495d3fe020c614d4fcbee05
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2019-01-26 23:27:02 +0100

    Switching Minigame kind of works (in TestSceneMichael). Still buggy
    interaction with the action queues is still messed up

---
+++ Assets/Code/Minigames/AbstractMinigame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// parent of minigames. Needed for showing up in the UnityEditor because IMinigame is not a GameObject.
+/// </summary>
+public abstract class AbstractMinigame : MonoBehaviour, IMinigame
+{
+    public abstract ActionQueueProperties GetActionQueueProperties();
+}
