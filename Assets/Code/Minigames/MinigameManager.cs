dc8a8750fb127e9f246d8327a47cc54fc7eaaa14
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2019-01-26 22:14:21 +0100

    Should be able to go to next minigame

---
+++ Assets/Code/Minigames/MinigameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    public Monster monster;
+    public IMinigame[] minigames;
+
+    private int currentMinigameIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CompleteCurrentMinigame()
+    {
+        currentMinigameIndex++;
+        if(currentMinigameIndex < minigames.Length)
+        {
+            Debug.Log(string.Format("preparing next minigame {0}!", currentMinigameIndex), this);
+            monster.SetCurrentActionProperties(minigames[currentMinigameIndex].GetActionQueueProperties());
+        }
+        else
+        {
+            //win the game!
+            Debug.Log("completed all minigames!", this);
+        }
+
+    }
+}
