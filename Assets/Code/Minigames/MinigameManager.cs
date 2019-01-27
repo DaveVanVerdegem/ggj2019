2043735131893af4dafb02c2fed2ae4e95766b06
Dave Van Verdegem <dave@pajamallama.be>
2019-01-27 12:14:54 +0100

    * Adjusted line endings.

--- Assets/Code/Minigames/MinigameManager.cs
+++ Assets/Code/Minigames/MinigameManager.cs
@@ -4,37 +4,35 @@ using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
-    public Monster monster;
-    public AbstractMinigame[] minigames;
+	public Monster monster;
+	public AbstractMinigame[] minigames;
 
-    private int currentMinigameIndex = 0;
+	private int currentMinigameIndex = 0;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        monster.SetCurrentActionProperties(minigames[currentMinigameIndex].GetActionQueueProperties());
-    }
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		monster.SetCurrentActionProperties(minigames[currentMinigameIndex].GetActionQueueProperties());
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	private void Update()
+	{
+	}
 
-    public void CompleteCurrentMinigame()
-    {
-        Debug.Log(string.Format("Minigame {0} completed!", currentMinigameIndex), this);
-        currentMinigameIndex++;
-        if(currentMinigameIndex < minigames.Length)
-        {
-            Debug.Log(string.Format("preparing next minigame {0}!", currentMinigameIndex), this);
-            monster.SetCurrentActionProperties(minigames[currentMinigameIndex].GetActionQueueProperties());
-        }
-        else
-        {
-            //win the game!
-            Debug.Log("completed all minigames!", this);
-        }
-
-    }
+	public void CompleteCurrentMinigame()
+	{
+		Debug.Log(string.Format("Minigame {0} completed!", currentMinigameIndex), this);
+		currentMinigameIndex++;
+		if (currentMinigameIndex < minigames.Length)
+		{
+			Debug.Log(string.Format("preparing next minigame {0}!", currentMinigameIndex), this);
+			monster.SetCurrentActionProperties(minigames[currentMinigameIndex].GetActionQueueProperties());
+		}
+		else
+		{
+			//win the game!
+			Debug.Log("completed all minigames!", this);
+		}
+	}
 }
--- Assets/Code/Touch/Dragable.cs
+++ Assets/Code/Touch/Dragable.cs
@@ -6,39 +6,38 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Dragable : MonoBehaviour
 {
-    public Color HighlightColor;
-    Color originalColor;
-    CircleCollider2D col;
-    SpriteRenderer spr;
-    // Start is called before the first frame update
-    void Start()
-    {
-        col = GetComponent<CircleCollider2D>();
-        spr= GetComponent<SpriteRenderer>();
-        originalColor = spr.color;
-    }
+	public Color HighlightColor;
+	private Color originalColor;
+	private CircleCollider2D col;
+	private SpriteRenderer spr;
+	// Start is called before the first frame update
+	private void Start()
+	{
+		col = GetComponent<CircleCollider2D>();
+		spr = GetComponent<SpriteRenderer>();
+		originalColor = spr.color;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	private void Update()
+	{
+	}
 
-    void OnMouseEnter()
-    {
-        originalColor = spr.color;
-        if (HighlightColor.a>0) spr.color = HighlightColor;
-    }
-    void OnMouseExit()
-    {
-        spr.color = originalColor;
-    }
-    void OnMouseDrag()
-    {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        objPosition.z = transform.position.z;
+	private void OnMouseEnter()
+	{
+		originalColor = spr.color;
+		if (HighlightColor.a > 0) spr.color = HighlightColor;
+	}
+	private void OnMouseExit()
+	{
+		spr.color = originalColor;
+	}
+	private void OnMouseDrag()
+	{
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		objPosition.z = transform.position.z;
 
-        transform.position = objPosition;
-    }
+		transform.position = objPosition;
+	}
 }
